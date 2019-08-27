18b731268e153000b877b111f8b0a4ff63674266
5z1punch <5z1punch>
2019-08-27 16:59:12 +0800

    init

---
+++ adb_controller/adb.py
@@ -0,0 +1,4 @@
+from subprocess import call, Popen, PIPE
+
+def start_server():
+    pass
---
+++ oat_repacker/adb_script/init_vm_env.sh
@@ -0,0 +1,5 @@
+stop && \
+setprop dalvik.vm.usejitprofiles false && \
+setprop dalvik.vm.usejit false && \
+setprop dalvik.vm.dexopt-flags v=n && \
+start
